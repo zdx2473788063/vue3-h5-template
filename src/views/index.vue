86b32532d0520318b9448ec4d6965c7942e936f6
翟东旭 <2473788063@qq.com>
2025-04-01 10:11:41 +0800

    新增公共图片域名配置

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,7 +1,10 @@
 <template>
-  1
+  <img :src="imgUrlData.imgUrl" alt=""/>
 </template>
 <script setup lang="ts">
+const imgUrlData = {
+  imgUrl: `${import.meta.env.VITE_IMAGE_URL}/example.png`
+}
 </script>
 <style scoped lang="scss">
 </style>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,6 +6,7 @@ interface ImportMetaEnv {
     readonly VITE_BUILD_SOURCEMAP: string;
     readonly VITE_BUILD_DROP_CONSOLE: string;
     readonly VITE_PUBLIC_URL: string;
+    readonly VITE_IMAGE_URL: string;
     readonly VITE_ROUTER_MODE: string;
     readonly VITE_SECRET_KEY: string;
 }
